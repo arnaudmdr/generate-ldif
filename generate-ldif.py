9e6ab940597483d60d300046d80ff0d97a8c7468
abouix <Arnaud@macbook-pro-de-arnaud.home>
2019-02-04 19:15:29 +0100

    [add] First commit, draft

---
+++ generate-ldif.py
@@ -0,0 +1,14 @@
+def generateLdifEntry(file, seed):
+    firstName="user"
+    lastName="test"
+    baseDn="ou=root,ou=fr"
+    seed=str(seed)
+    file.write("dn: uid=" + firstName + seed + "." + lastName + "," + baseDn + "\n")
+    file.write("uid: " + firstName + seed + "." + lastName + "\n")
+    file.write("\n")
+
+
+file = open("file.ldif","w+")
+for i in range(20):
+    generateLdifEntry(file,i)
+file.close()
